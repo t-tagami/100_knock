6fb22ef07cb43ec4dd8755696d641e5cf59e0338
t-tagami <tagami@ecei.tohoku.ac.jp>
2019-02-20 12:33:18 +0900

    Fixed Chap_03

--- Chapter_03/src/knock_20.py
+++ Chapter_03/src/knock_20.py
@@ -2,14 +2,15 @@
 # 20. JSONデータの読み込み
 # Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
 
-import gzip, json
+import gzip
+import simplejson as json
 
 def main():
     print(extract_uk())
 
 def extract_uk():
-    with gzip.open('../data/jawiki-country.json.gz', 'rt') as f20:
-        for line in f20:
+    with gzip.open('../data/jawiki-country.json.gz', 'rt') as f:
+        for line in f:
             if (json.loads(line)['title'] == 'イギリス'): return(json.loads(line)['text'])
 
 
